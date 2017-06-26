df34b01e7d52b97ca7ef6debe6451369b8154739
muneeba1 <mkhatoo1@mail.depaul.edu>
2017-06-26 13:10:40 -0500

    Initial Commit

---
+++ Makestagram/Makestagram/Controllers/LoginViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewController.swift
+//  Makestagram
+//
+//  Created by Muneeba Khatoon on 6/26/17.
+//  Copyright © 2017 Muneeba Khatoon. All rights reserved.
+//
+
+import Foundation
